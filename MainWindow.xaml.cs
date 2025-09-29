5c27db76fd48e80a478d3079b5f529788b9d69aa
Crusiador <cp.gaming@gmx.ch>
2025-09-29 22:29:15 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+
+namespace MyWpfApp;
+
+public partial class MainWindow : Window
+{
+    private bool running = false;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+    private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+    {
+        running = !running; 
+        if (running)
+        {
+            tbStatus.Text = tbInput.Text;
+            btnToggleRun.Content = "Refresh";
+        }
+        else
+        {
+            tbStatus.Text = tbInput.Text;
+            btnToggleRun.Content = "Refresh";
+        }
+    }
+
+    private void btnProcessInput_Click(object sender, RoutedEventArgs e)
+    {
+        string inputText = tbInput.Text;
+        char c1 = tbInput.Text[0];
+        char c2 = tbInput.Text[1];
+        char c3 = tbInput.Text[2];
+        char c4 = tbInput.Text[3];
+        char c5 = tbInput.Text[4];
+        char c6 = tbInput.Text[5];
+        char c7 = tbInput.Text[6];
+
+        inputText = ToString(c1, c4, c3, c2, c5, c6, c7);
+    }
+
+    private string ToString(char c1, char c4, char c3, char c2, char c5, char c6, char c7)
+    {
+        return $"{c1}, {c4}, {c3}, {c2}, {c5}, {c6}, {c7}";
+    }
+
+}
